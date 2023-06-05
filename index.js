7082dd18365903ac8dd864f8be422c486e22de49
SauceX <mahmoudzak4@gmail.com>
2023-06-05 19:46:17 +0300

    chore: headless

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
+        headless: true,
         args: ['--no-sandbox', '--disable-setuid-sandbox'],
     },
 });
